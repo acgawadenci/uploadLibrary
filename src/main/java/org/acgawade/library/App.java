cf6c645b0d180f11146f260f223f839476d1ce5e
Achyut Gawade <gawade.achyut96@gmail.com>
2023-04-24 13:06:54 +0100

    Initial COmmit

---
+++ src/main/java/org/acgawade/library/App.java
@@ -0,0 +1,13 @@
+package org.acgawade.library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
